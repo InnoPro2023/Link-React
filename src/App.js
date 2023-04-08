44fbd56c5085f3740e845ff774468ad93ef4ea73
DESKTOP-ERMH4GV\DELL <scottnguyen2411@gmail.com>
2023-04-08 09:29:35 +0700

    new2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './components/Home';
+import  Home  from './components/Home';
 import { About } from './components/About';
 import { Navbar } from './components/Navbar';
 
